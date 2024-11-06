388aaf9c83c9cedd88c694afc734142e43b7da0f
Chikage Molander <chikage.t.molander@gmail.com>
2024-11-06 11:18:29 +0100

    add utc into formatTime

--- lib/formatTime.ts
+++ lib/formatTime.ts
@@ -1,24 +1,29 @@
 // Time format function for feed
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
 
 export default function formatTime(date: string): string {
-  const postDate = dayjs(date);
-  const now = dayjs();
+  dayjs.extend(utc)
+  const postDate = dayjs.utc(date);
+  const now = dayjs.utc();
 
   const diffInMinutes = now.diff(postDate, 'minute');
-  const diffInHours = now.diff(postDate, 'hour');
-  const diffInDays = now.diff(postDate, 'day');
-  const diffInWeeks = now.diff(postDate, 'week');
+  
 
-  if (diffInMinutes < 60) {
+  if (diffInMinutes < 1) {
+    return `just now`; 
+  } else if (diffInMinutes < 60) {
     return `${diffInMinutes}m`;
-  } else if (diffInHours < 24) {
-    return `${diffInHours}h`;
-  } else if (diffInDays < 7) {
+  } else if (diffInMinutes < 1440) { 
+    const diffInHours = Math.floor(diffInMinutes / 60);
+    return `${diffInHours}h`; 
+  } else if (diffInMinutes < 10080) { 
+    const diffInDays = Math.floor(diffInMinutes / 1440);
     return `${diffInDays}d`;
-  } else if (diffInWeeks < 4) {
+  } else if (diffInMinutes < 40320) { 
+    const diffInWeeks = Math.floor(diffInMinutes / 10080);
     return `${diffInWeeks}w`;
   } else {
-    return postDate.format('YYYY-MM-DD');
+    return postDate.format('YYYY-MM-DD'); 
   }
 }
